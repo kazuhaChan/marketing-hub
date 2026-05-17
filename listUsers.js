2165c1500d6ac557a30e5ed8b17bad76521aa9fe
kazuhaChan <nddung2609@gmail.com>
2026-05-17 15:39:45 +0700

    Add script to list all registered users on production

---
+++ listUsers.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const User = require('./src/models/User');
+
+const listUsers = async () => {
+  try {
+    console.log('Connecting to MongoDB...');
+    await mongoose.connect(process.env.MONGO_URI);
+    
+    const users = await User.find({}).select('-password');
+    console.log('\n=== ALL REGISTERED USERS ===');
+    if (users.length === 0) {
+      console.log('No users found in the database.');
+    } else {
+      users.forEach((u, i) => {
+        console.log(`${i + 1}. [${u.role}] Username: ${u.username} | Email: ${u.email}`);
+      });
+    }
+    console.log('============================\n');
+  } catch (err) {
+    console.error('Error:', err);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+listUsers();
